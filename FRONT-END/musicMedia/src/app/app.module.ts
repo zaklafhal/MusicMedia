4fc585e7cbdd168a28906e34d00c33cb8a342df5
zakaria Lafhal <57017426+zaklafhal@users.noreply.github.com>
2020-07-06 11:10:02 -0400

    Init the Input component

--- FRONT-END/musicMedia/src/app/app.module.ts
+++ FRONT-END/musicMedia/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
--- FRONT-END/musicMedia/src/app/login/login.component.ts
+++ FRONT-END/musicMedia/src/app/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginRequest } from '../dto/loginRequest';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginRequest: LoginRequest = {
+    email: 'myemail@gmail.com',
+    password: 'Passw0rd!',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

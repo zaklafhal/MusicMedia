d8259e631e32e36d3bac22430f10184f049a040f
zakaria Lafhal <57017426+zaklafhal@users.noreply.github.com>
2020-07-07 15:41:44 -0400

    🛣 Init the app-routing Module

--- FRONT-END/musicMedia/src/app/app.module.ts
+++ FRONT-END/musicMedia/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { InputComponent } from './input/input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, InputComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })

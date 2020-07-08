1eac387da126d5f44ffe661911a6372ebd1c079f
zakaria Lafhal <57017426+zaklafhal@users.noreply.github.com>
2020-07-08 18:30:59 -0400

    💻 Set-up the login feature and Init the storage service

--- FRONT-END/musicMedia/src/app/login/login.component.ts
+++ FRONT-END/musicMedia/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginRequest } from '../dto/loginRequest';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from './../user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,20 +11,27 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginRequest: LoginRequest = {
-    email: 'myemail@gmail.com',
+    email: 'test@test.ca',
     password: 'Passw0rd!',
   };
   public form = this.formBuilder.group({
     email: ['', [Validators.email, Validators.required]],
     password: ['', Validators.required],
   });
-  constructor(private formBuilder: FormBuilder, private router : Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService
+  ) {}
   errorRequired: string = 'This field is required';
   errorEmail: string =
     this.form.controls.email.value === ''
       ? this.errorRequired
       : 'Invalid email address';
   signIn(form): void {
+    this.userService
+      .login(this.loginRequest)
+      .subscribe((res) => console.log(res));
     this.router.navigate(['main']);
   }
   ngOnInit(): void {}

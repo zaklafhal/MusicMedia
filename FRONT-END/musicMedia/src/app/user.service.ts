1b1830e129d764ff5b833218a47b0d0f238c9c5e
zakaria Lafhal <57017426+zaklafhal@users.noreply.github.com>
2020-07-13 14:28:29 -0400

    🚀 Add the environment variabes in the angular project

--- FRONT-END/musicMedia/src/app/user.service.ts
+++ FRONT-END/musicMedia/src/app/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LoginRequest } from './dto/loginRequest';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,7 @@ export class UserService {
   constructor(private http: HttpClient) {
     
   }
-  private url = 'https://localhost:44314/';
-  private loginUrl = this.url + 'api/token';
+  private loginUrl = environment.endpoint + 'token';
   login(loginRequest :LoginRequest) : Observable<string>{
     return this.http.post<string>(this.loginUrl,loginRequest);
   }

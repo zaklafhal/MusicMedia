55faa9bba458f72c806ef133627c72125de96492
Zakaria <zakarialafhal98@gmail.com>
2020-07-17 13:30:47 -0400

    🎹 Set-up the spotify service

--- FRONT-END/musicMedia/src/app/spotify.service.ts
+++ FRONT-END/musicMedia/src/app/spotify.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpotifyService {
+  constructor(private http: HttpClient) {}
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // client or network error
+    } else {
+      // Backend error
+    }
 
-  constructor() { }
+    return throwError('erro');
+  }
+
+  searchArtist(artistName: string): Observable<any> {
+    const url = `https://api.spotify.com/v1/search?query=${artistName}&offset=0&limit=20&type=artist`;
+    return this.http.get<any>(url).pipe(
+      catchError(this.handleError),
+      map((res) => res.json)
+    );
+  }
 }
--- FRONT-END/musicMedia/src/app/user.service.ts
+++ FRONT-END/musicMedia/src/app/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LoginRequest } from './dto/loginRequest';
 import { environment } from './../environments/environment';

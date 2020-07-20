5088995735eb0370601dc006755cbb8da6b72e1b
Zakaria <zakarialafhal98@gmail.com>
2020-07-20 16:42:29 -0400

    🗝 Set-up the getAuth function in the spotify service

--- FRONT-END/musicMedia/src/app/spotify.service.ts
+++ FRONT-END/musicMedia/src/app/spotify.service.ts
@@ -5,7 +5,7 @@ import {
   HttpHeaders,
   HttpErrorResponse,
 } from '@angular/common/http';
-import config from './config.json'
+import config from './config.json';
 import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
@@ -22,7 +22,27 @@ export class SpotifyService {
 
     return throwError('erro');
   }
+  getAuth(): Observable<any> {
+    const headers = new HttpHeaders();
+    headers.append(
+      'Authorization',
+      'Basic ' + btoa(config.client_id + ':' + config.client_secret)
+    );
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
+    const params: URLSearchParams = new URLSearchParams();
+    params.set('grant_type', 'client_credentials');
+    const body = params.toString();
+
+    return this.http
+      .post<any>('https://accounts.spotify.com/api/token', body, {
+        headers: headers,
+      })
+      .pipe(
+        catchError(this.handleError),
+        map((res) => res.json)
+      );
+  }
 
   searchArtist(artistName: string): Observable<any> {
     const url = `https://api.spotify.com/v1/search?query=${artistName}&offset=0&limit=20&type=artist`;

4e888585f14f98ffa6b4958271d8b5bf4e2a66b3
Zakaria <zakarialafhal98@gmail.com>
2020-07-21 21:36:07 -0400

    ✔ Load the image dynamicly

--- FRONT-END/musicMedia/src/app/main/main.component.ts
+++ FRONT-END/musicMedia/src/app/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { SpotifyService } from './../spotify.service';
+import { Artist } from './../model/artist';
 
 @Component({
   selector: 'app-main',
@@ -11,6 +12,7 @@ export class MainComponent implements OnInit {
   public form = this.formBuilder.group({
     artisteName: ['', []],
   });
+  public artist: Artist;
   constructor(
     private formBuilder: FormBuilder,
     private spotify: SpotifyService
@@ -18,7 +20,7 @@ export class MainComponent implements OnInit {
 
   search(form: FormGroup) {
     const { value: artiste } = form.controls.artisteName;
-    this.spotify.searchArtist(artiste).subscribe((res) => console.log(res))
+    this.spotify.searchArtist(artiste).subscribe((res) => this.artist = res);
   }
   ngOnInit(): void {
     this.getToken();

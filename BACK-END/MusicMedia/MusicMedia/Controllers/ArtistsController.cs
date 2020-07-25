68d2efbcc6e1dfb4b950350982faa1df40e51f2c
Zakaria <zakarialafhal98@gmail.com>
2020-07-25 13:57:19 -0400

    ✔ Add the artist controller

---
+++ BACK-END/MusicMedia/MusicMedia/Controllers/ArtistsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MusicMedia.Data;
+using MusicMedia.Models;
+
+namespace MusicMedia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        public ArtistsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+    }
+}

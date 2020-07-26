3c94e659d5b87a752b309be9b49fc7f2ccda68f7
Zakaria <zakarialafhal98@gmail.com>
2020-07-25 20:20:19 -0400

    🔐 Add Validation to the ArtistDto model

--- BACK-END/MusicMedia/MusicMedia/Models/Dto/ArtistDto.cs
+++ BACK-END/MusicMedia/MusicMedia/Models/Dto/ArtistDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace MusicMedia.Models.Dto
 {
     public class ArtistDto
     {
+        [Required]
         public string SpotifyId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Image { get; set; }
     }
 }

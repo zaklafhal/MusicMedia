b63f425ad97ca63544a4c1df1e49bfaf9495f908
Zakaria <zakarialafhal98@gmail.com>
2020-07-25 20:54:30 -0400

    💻 Add the addArtist function in the angular project

--- FRONT-END/musicMedia/src/app/services/artist.service.ts
+++ FRONT-END/musicMedia/src/app/services/artist.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LoginRequest } from './../dto/loginRequest';
+import { environment } from './../../environments/environment';
+import { Artist } from '../model/artist';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistService {
 
-  constructor() { }
+  constructor(private http : HttpClient, private storage: StorageService) { }
+  private endpoint = `${environment.endpoint} + artists` 
+
+  addArtist(artist: Artist): void{
+    if(this.storage.user){
+      const headers = new HttpHeaders({
+        Authorization:
+          'Bearer  ' + this.storage.getToken(),
+        'Content-Type': 'application/x-www-form-urlencoded;',
+      });
+      this.http.post(this.endpoint,artist, {headers: headers})
+    }
+  }
 }

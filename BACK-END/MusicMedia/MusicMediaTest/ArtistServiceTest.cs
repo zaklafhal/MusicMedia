cc3c0aeb59c0fd1acc2a1aab794bf97e486cc0e8
Zakaria <zakarialafhal98@gmail.com>
2020-07-30 23:18:22 -0400

    🐱‍💻 Add the ValidateArtistWithModelNull case

--- BACK-END/MusicMedia/MusicMediaTest/ArtistServiceTest.cs
+++ BACK-END/MusicMedia/MusicMediaTest/ArtistServiceTest.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using MusicMedia.Data;
+using MusicMedia.Models;
+using MusicMedia.Models.Dto;
+using MusicMedia.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace MusicMediaTest
 {
-    class ArtistServiceTest
+    public class ArtistServiceTest
+
+
     {
+        [Fact]
+        public void TestValidateArtistWithModelNull()
+        {
+            var dbOptionsBuilder = new DbContextOptionsBuilder().UseInMemoryDatabase("music_media_test)");
+
+            var context = new ApplicationDbContext(dbOptionsBuilder.Options);
+
+            var service = new ArtistService(context);
+
+            ArtistDto model = null;
+
+            var user = new ApplicationUser();
+
+            Assert.Throws<Exception>(() => service.ValidateArtist(model, user));
+            
+        }
     }
 }

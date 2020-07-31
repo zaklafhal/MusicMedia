9768b4acb0a60f4f0bea18bf352d3fb3b6dd8fda
Zakaria <zakarialafhal98@gmail.com>
2020-07-31 18:15:00 -0400

    Implement the TokenService

--- BACK-END/MusicMedia/MusicMedia/Services/ITokenService.cs
+++ BACK-END/MusicMedia/MusicMedia/Services/ITokenService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicMedia.Models.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ namespace MusicMedia.Services
 {
     public interface ITokenService
     {
-
+        Task<dynamic> GenerateToken(string email);
+        Task<bool> IsValidUser(LoginRequest loginRequest);
     }
 }
--- BACK-END/MusicMedia/MusicMedia/Services/TokenService.cs
+++ BACK-END/MusicMedia/MusicMedia/Services/TokenService.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using MusicMedia.Models;
+using MusicMedia.Models.Dto;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MusicMedia.Services
 {
-    interface TokenService
+    public class TokenService : ITokenService
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public TokenService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<dynamic> GenerateToken(string email)
+        {
+            var user = await _userManager.FindByNameAsync(email);
+            var userInfos = new UserInfo(user);
+            var claims = new List<Claim> {
+                new Claim("user" , userInfos.ToString()),
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString())
+            };
+            var token = new JwtSecurityToken(
+                new JwtHeader(
+                    new SigningCredentials(
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes("uhsnsdsdheweqys87272871hwn21y1")),
+                        SecurityAlgorithms.HmacSha256)),
+                        new JwtPayload(claims));
+            var result = new { Access_Token = new JwtSecurityTokenHandler().WriteToken(token) };
+            return result;
+        }
+
+        public async Task<bool> IsValidUser(LoginRequest loginRequest)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

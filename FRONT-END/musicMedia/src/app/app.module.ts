db53748feae228c443c68d682dda76ba3902ed8b
Zakaria <zakarialafhal98@gmail.com>
2020-08-04 23:13:29 -0400

    ➕ Add the register component

--- FRONT-END/musicMedia/src/app/app.module.ts
+++ FRONT-END/musicMedia/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { InputComponent } from './input/input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, InputComponent, MainComponent],
+  declarations: [AppComponent, LoginComponent, InputComponent, MainComponent, RegisterComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],

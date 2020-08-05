c365e903cb91d188566b835015b011f1bf260289
Zakaria <zakarialafhal98@gmail.com>
2020-08-04 23:26:15 -0400

    💻 Set-up the form for the register

--- FRONT-END/musicMedia/src/app/register/register.component.ts
+++ FRONT-END/musicMedia/src/app/register/register.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public form = this.formBuilder.group({
+    email: ['', [Validators.email, Validators.required]],
+    name: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  });
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

a9fa46091364ca92cad96c6217e9d0fe2ff04469
Zakaria <zakarialafhal98@gmail.com>
2020-08-04 23:16:28 -0400

    📍Set-up the routes for the register component

--- FRONT-END/musicMedia/src/app/app-routing.module.ts
+++ FRONT-END/musicMedia/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'main', component: MainComponent },
 ];
 

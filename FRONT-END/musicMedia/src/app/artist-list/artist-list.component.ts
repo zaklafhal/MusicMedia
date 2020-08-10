4933e2320a05dca63b73491523bcc83716b058c0
Zakaria <zakarialafhal98@gmail.com>
2020-08-10 16:06:34 -0400

    🐱‍💻 Display the artists list

--- FRONT-END/musicMedia/src/app/artist-list/artist-list.component.ts
+++ FRONT-END/musicMedia/src/app/artist-list/artist-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Artist } from '../model/artist';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-artist-list',
   templateUrl: './artist-list.component.html',
-  styleUrls: ['./artist-list.component.css']
+  styleUrls: ['./artist-list.component.css'],
 })
 export class ArtistListComponent implements OnInit {
+  artists: Artist[];
+  constructor(private storage: StorageService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getArtists(): void {
+    this.artists = this.storage.getArtists();
   }
 
+  ngOnInit(): void {
+    this.getArtists();
+  }
 }

8bd17a25a8899d267b41a1499300b9e21f466c8c
Zakaria <zakarialafhal98@gmail.com>
2020-08-18 15:56:02 -0400

    📍 Add the album component to the route module

--- FRONT-END/musicMedia/src/app/app-routing.module.ts
+++ FRONT-END/musicMedia/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './register/register.component';
 import { ArtistListComponent } from './artist-list/artist-list.component';
+import { AlbumComponent } from './album/album.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'main', component: MainComponent },
   { path: 'myArtists', component: ArtistListComponent },
+  { path: 'album/:id', component: AlbumComponent },
 ];
 
 @NgModule({

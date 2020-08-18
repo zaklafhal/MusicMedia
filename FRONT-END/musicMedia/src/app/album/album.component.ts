f6835d47325dc4f9e0a46275545906ee1eb7b2a9
Zakaria <zakarialafhal98@gmail.com>
2020-08-18 16:08:06 -0400

    🐱‍👤 Add the getAlbums to the album component

--- FRONT-END/musicMedia/src/app/album/album.component.ts
+++ FRONT-END/musicMedia/src/app/album/album.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from '../services/spotify.service';
 
 @Component({
   selector: 'app-album',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute, private spotify: SpotifyService) { }
+  private artistId = this.route.snapshot.paramMap.get('id');
+  
   ngOnInit(): void {
+    this.getAlbums();
+  }
+  getAlbums() {
+    this.spotify
+      .getAlbums(this.artistId)
+      .subscribe((res) => console.log(res));
   }
 
 }
